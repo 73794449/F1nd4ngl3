d546059842263a69fcf63352432a87b8bfe27627
Ivan Terentiev <t3ry444y@gmail.com>
2022-01-06 08:50:45 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,385 @@
+﻿namespace FindCinABC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AX = new System.Windows.Forms.TextBox();
+            this.AY = new System.Windows.Forms.TextBox();
+            this.AZ = new System.Windows.Forms.TextBox();
+            this.AResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BX = new System.Windows.Forms.TextBox();
+            this.BY = new System.Windows.Forms.TextBox();
+            this.BZ = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CX = new System.Windows.Forms.TextBox();
+            this.CY = new System.Windows.Forms.TextBox();
+            this.CZ = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.BResult = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.CResult = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.RESCOS = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // AX
+            // 
+            this.AX.Location = new System.Drawing.Point(15, 25);
+            this.AX.Name = "AX";
+            this.AX.Size = new System.Drawing.Size(38, 20);
+            this.AX.TabIndex = 0;
+            // 
+            // AY
+            // 
+            this.AY.Location = new System.Drawing.Point(15, 51);
+            this.AY.Name = "AY";
+            this.AY.Size = new System.Drawing.Size(38, 20);
+            this.AY.TabIndex = 1;
+            // 
+            // AZ
+            // 
+            this.AZ.Location = new System.Drawing.Point(15, 77);
+            this.AZ.Name = "AZ";
+            this.AZ.Size = new System.Drawing.Size(38, 20);
+            this.AZ.TabIndex = 2;
+            // 
+            // AResult
+            // 
+            this.AResult.Location = new System.Drawing.Point(12, 129);
+            this.AResult.Name = "AResult";
+            this.AResult.Size = new System.Drawing.Size(60, 20);
+            this.AResult.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Input A";
+            // 
+            // BX
+            // 
+            this.BX.Location = new System.Drawing.Point(59, 25);
+            this.BX.Name = "BX";
+            this.BX.Size = new System.Drawing.Size(38, 20);
+            this.BX.TabIndex = 0;
+            // 
+            // BY
+            // 
+            this.BY.Location = new System.Drawing.Point(59, 51);
+            this.BY.Name = "BY";
+            this.BY.Size = new System.Drawing.Size(38, 20);
+            this.BY.TabIndex = 1;
+            // 
+            // BZ
+            // 
+            this.BZ.Location = new System.Drawing.Point(59, 77);
+            this.BZ.Name = "BZ";
+            this.BZ.Size = new System.Drawing.Size(38, 20);
+            this.BZ.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Input B";
+            // 
+            // CX
+            // 
+            this.CX.Location = new System.Drawing.Point(103, 25);
+            this.CX.Name = "CX";
+            this.CX.Size = new System.Drawing.Size(38, 20);
+            this.CX.TabIndex = 0;
+            // 
+            // CY
+            // 
+            this.CY.Location = new System.Drawing.Point(103, 51);
+            this.CY.Name = "CY";
+            this.CY.Size = new System.Drawing.Size(38, 20);
+            this.CY.TabIndex = 1;
+            // 
+            // CZ
+            // 
+            this.CZ.Location = new System.Drawing.Point(103, 77);
+            this.CZ.Name = "CZ";
+            this.CZ.Size = new System.Drawing.Size(38, 20);
+            this.CZ.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(100, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Input C";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Angle A";
+            // 
+            // BResult
+            // 
+            this.BResult.Location = new System.Drawing.Point(12, 168);
+            this.BResult.Name = "BResult";
+            this.BResult.Size = new System.Drawing.Size(60, 20);
+            this.BResult.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Angle B";
+            // 
+            // CResult
+            // 
+            this.CResult.Location = new System.Drawing.Point(12, 207);
+            this.CResult.Name = "CResult";
+            this.CResult.Size = new System.Drawing.Size(60, 20);
+            this.CResult.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 191);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Angle C";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(78, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 20);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(78, 168);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 20);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Calculate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(78, 129);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(60, 20);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Calculate";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(144, 207);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(405, 20);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "ALWAYS ON TOP";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(159, 25);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 7;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(159, 45);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 8;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(159, 65);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 9;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(159, 85);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 13);
+            this.label10.TabIndex = 10;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(159, 105);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 13);
+            this.label11.TabIndex = 11;
+            // 
+            // RESCOS
+            // 
+            this.RESCOS.AutoSize = true;
+            this.RESCOS.Location = new System.Drawing.Point(159, 125);
+            this.RESCOS.Name = "RESCOS";
+            this.RESCOS.Size = new System.Drawing.Size(0, 13);
+            this.RESCOS.TabIndex = 12;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(281, 105);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(126, 13);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "You will see solution here";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(427, 230);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(122, 13);
+            this.label13.TabIndex = 14;
+            this.label13.Text = "Created by T3RY4 2022";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 245);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.RESCOS);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CResult);
+            this.Controls.Add(this.BResult);
+            this.Controls.Add(this.AResult);
+            this.Controls.Add(this.CZ);
+            this.Controls.Add(this.CY);
+            this.Controls.Add(this.BZ);
+            this.Controls.Add(this.BY);
+            this.Controls.Add(this.CX);
+            this.Controls.Add(this.AZ);
+            this.Controls.Add(this.BX);
+            this.Controls.Add(this.AY);
+            this.Controls.Add(this.AX);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "F1nd4ngl3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox AX;
+        private System.Windows.Forms.TextBox AY;
+        private System.Windows.Forms.TextBox AZ;
+        private System.Windows.Forms.TextBox AResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox BX;
+        private System.Windows.Forms.TextBox BY;
+        private System.Windows.Forms.TextBox BZ;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox CX;
+        private System.Windows.Forms.TextBox CY;
+        private System.Windows.Forms.TextBox CZ;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox BResult;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox CResult;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label RESCOS;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Windows.Forms;
+namespace FindCinABC
+{
+    public partial class Form1 : Form
+    {
+        public Form1() => InitializeComponent();
+
+        private void button4_Click(object sender, System.EventArgs e)
+        {
+            if (this.TopMost.Equals(false))
+            {
+                this.TopMost = true;
+                button4.BackColor = System.Drawing.Color.Aqua;
+            }
+            else
+            {
+                this.TopMost = false;
+                button4.BackColor = System.Drawing.Color.White;
+            }
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            double[] BCvect = new double[3] { Convert.ToInt32(CX.Text)
+                - Convert.ToInt32(BX.Text), Convert.ToInt32(CY.Text)
+                - Convert.ToInt32(BY.Text), Convert.ToInt32(CZ.Text)
+                - Convert.ToInt32(BZ.Text) };
+            double[] ACvect = new double[3] { Convert.ToInt32(CX.Text)
+                - Convert.ToInt32(AX.Text), Convert.ToInt32(CY.Text)
+                - Convert.ToInt32(AY.Text), Convert.ToInt32(CZ.Text)
+                - Convert.ToInt32(AZ.Text) };
+            double BCmodule = Math.Sqrt(Math.Pow(BCvect[0], 2) + Math.Pow(BCvect[1], 2) + Math.Pow(BCvect[2], 2));
+            double ACmodule = Math.Sqrt(Math.Pow(ACvect[0], 2) + Math.Pow(ACvect[1], 2) + Math.Pow(ACvect[2], 2));
+            double result = Math.Acos((BCvect[0] * ACvect[0] + BCvect[1] * ACvect[1] + BCvect[2] * ACvect[2]) / (BCmodule * ACmodule));
+            label7.Text = "vector BC = {"
+                + BCvect[0].ToString()
+                + ", "
+                + BCvect[1].ToString()
+                + ", "
+                + BCvect[2].ToString()
+                + "}";
+            label8.Text = "vector AC = {"
+                + ACvect[0].ToString()
+                + ", "
+                + ACvect[1].ToString()
+                + ", "
+                + ACvect[2].ToString()
+                + "}";
+            label9.Text = "module vector BC = sqrt("
+                + Math.Pow(BCvect[0], 2).ToString()
+                + " + "
+                + Math.Pow(BCvect[1], 2).ToString()
+                + " + "
+                + Math.Pow(BCvect[2], 2).ToString()
+                + ") = "
+                + BCmodule.ToString();
+            label10.Text = "module vector AC = sqrt("
+                + Math.Pow(ACvect[0], 2).ToString()
+                + " + "
+                + Math.Pow(ACvect[1], 2).ToString()
+                + " + "
+                + Math.Pow(ACvect[2], 2).ToString()
+                + ") = "
+                + ACmodule.ToString();
+            label11.Text = "vector BC * vector AC = "
+                + BCvect[0].ToString()
+                + " * "
+                + ACvect[0].ToString()
+                + " + "
+                + BCvect[1].ToString()
+                + " * "
+                + ACvect[1].ToString()
+                + " + "
+                + BCvect[2].ToString()
+                + " * "
+                + ACvect[2].ToString()
+                + " = "
+                + (BCvect[0] * ACvect[0] + BCvect[1] * ACvect[1] + (BCvect[2] * ACvect[2])).ToString();
+            RESCOS.Text = "cos C = (vector BC * vector AC)/\n             (module vector BC * module vector AC)" + "\n          = " + ((BCvect[0] * ACvect[0] + BCvect[1] * ACvect[1] + BCvect[2] * ACvect[2]) / (BCmodule * ACmodule)).ToString();
+            CResult.Text = result.ToString();
+            label12.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double[] BAvect = new double[3] { Convert.ToInt32(AX.Text)
+                - Convert.ToInt32(BX.Text), Convert.ToInt32(AY.Text)
+                - Convert.ToInt32(BY.Text), Convert.ToInt32(AZ.Text)
+                - Convert.ToInt32(BZ.Text) };
+            double[] CAvect = new double[3] { Convert.ToInt32(AX.Text)
+                - Convert.ToInt32(CX.Text), Convert.ToInt32(AY.Text)
+                - Convert.ToInt32(CY.Text), Convert.ToInt32(AZ.Text)
+                - Convert.ToInt32(CZ.Text) };
+            double BAmodule = Math.Sqrt(Math.Pow(BAvect[0], 2) + Math.Pow(BAvect[1], 2) + Math.Pow(BAvect[2], 2));
+            double CAmodule = Math.Sqrt(Math.Pow(CAvect[0], 2) + Math.Pow(CAvect[1], 2) + Math.Pow(CAvect[2], 2));
+            double result = Math.Acos((BAvect[0] * CAvect[0] + BAvect[1] * CAvect[1] + BAvect[2] * CAvect[2]) / (BAmodule * CAmodule));
+            label7.Text = "vector BA = {"
+                + BAvect[0].ToString()
+                + ", "
+                + BAvect[1].ToString()
+                + ", "
+                + BAvect[2].ToString()
+                + "}";
+            label8.Text = "vector CA = {"
+                + CAvect[0].ToString()
+                + ", "
+                + CAvect[1].ToString()
+                + ", "
+                + CAvect[2].ToString()
+                + "}";
+            label9.Text = "module vector BA = sqrt("
+                + Math.Pow(BAvect[0], 2).ToString()
+                + " + "
+                + Math.Pow(BAvect[1], 2).ToString()
+                + " + "
+                + Math.Pow(BAvect[2], 2).ToString()
+                + ") = "
+                + BAmodule.ToString();
+            label10.Text = "module vector CA = sqrt("
+                + Math.Pow(CAvect[0], 2).ToString()
+                + " + "
+                + Math.Pow(CAvect[1], 2).ToString()
+                + " + "
+                + Math.Pow(CAvect[2], 2).ToString()
+                + ") = "
+                + CAmodule.ToString();
+            label11.Text = "vector BA * vector CA = "
+                + BAvect[0].ToString()
+                + " * "
+                + CAvect[0].ToString()
+                + " + "
+                + BAvect[1].ToString()
+                + " * "
+                + CAvect[1].ToString()
+                + " + "
+                + BAvect[2].ToString()
+                + " * "
+                + CAvect[2].ToString()
+                + " = "
+                + (BAvect[0] * CAvect[0] + BAvect[1] * CAvect[1] + (BAvect[2] * CAvect[2])).ToString();
+            RESCOS.Text = "cos A = (vector BA * vector CA)/\n             (module vector BA * module vector CA)" + "\n          = " + ((BAvect[0] * CAvect[0] + BAvect[1] * CAvect[1] + BAvect[2] * CAvect[2]) / (BAmodule * CAmodule)).ToString();
+            AResult.Text = result.ToString();
+            label12.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double[] ABvect = new double[3] { Convert.ToInt32(AX.Text)
+                - Convert.ToInt32(BX.Text), Convert.ToInt32(AY.Text)
+                - Convert.ToInt32(BY.Text), Convert.ToInt32(AZ.Text)
+                - Convert.ToInt32(BZ.Text) };
+            double[] CBvect = new double[3] { Convert.ToInt32(AX.Text)
+                - Convert.ToInt32(CX.Text), Convert.ToInt32(AY.Text)
+                - Convert.ToInt32(CY.Text), Convert.ToInt32(AZ.Text)
+                - Convert.ToInt32(CZ.Text) };
+            double ABmodule = Math.Sqrt(Math.Pow(ABvect[0], 2) + Math.Pow(ABvect[1], 2) + Math.Pow(ABvect[2], 2));
+            double CBmodule = Math.Sqrt(Math.Pow(CBvect[0], 2) + Math.Pow(CBvect[1], 2) + Math.Pow(CBvect[2], 2));
+            double result = Math.Acos((ABvect[0] * CBvect[0] + ABvect[1] * CBvect[1] + ABvect[2] * CBvect[2]) / (ABmodule * CBmodule));
+            label7.Text = "vector AB = {"
+                + ABvect[0].ToString()
+                + ", "
+                + ABvect[1].ToString()
+                + ", "
+                + ABvect[2].ToString()
+                + "}";
+            label8.Text = "vector CB = {"
+                + CBvect[0].ToString()
+                + ", "
+                + CBvect[1].ToString()
+                + ", "
+                + CBvect[2].ToString()
+                + "}";
+            label9.Text = "module vector AB = sqrt("
+                + Math.Pow(ABvect[0], 2).ToString()
+                + " + "
+                + Math.Pow(ABvect[1], 2).ToString()
+                + " + "
+                + Math.Pow(ABvect[2], 2).ToString()
+                + ") = "
+                + ABmodule.ToString();
+            label10.Text = "module vector CB = sqrt("
+                + Math.Pow(CBvect[0], 2).ToString()
+                + " + "
+                + Math.Pow(CBvect[1], 2).ToString()
+                + " + "
+                + Math.Pow(CBvect[2], 2).ToString()
+                + ") = "
+                + CBmodule.ToString();
+            label11.Text = "vector AB * vector CB = "
+                + ABvect[0].ToString()
+                + " * "
+                + CBvect[0].ToString()
+                + " + "
+                + ABvect[1].ToString()
+                + " * "
+                + CBvect[1].ToString()
+                + " + "
+                + ABvect[2].ToString()
+                + " * "
+                + CBvect[2].ToString()
+                + " = "
+                + (ABvect[0] * CBvect[0] + ABvect[1] * CBvect[1] + (ABvect[2] * CBvect[2])).ToString();
+            RESCOS.Text = "cos A = (vector AB * vector CB)/\n             (module vector AB * module vector CB)" + "\n          = " + ((ABvect[0] * CBvect[0] + ABvect[1] * CBvect[1] + ABvect[2] * CBvect[2]) / (ABmodule * CBmodule)).ToString();
+            BResult.Text = result.ToString();
+            label12.Text = "";
+        }
+    }
+}
